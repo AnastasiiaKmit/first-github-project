499ef034d5ad9d554436ea9ebe4e86a7c8e5fa3b
AnastasiiaKmit <anastasiiakmit99@gmail.com>
2022-09-03 19:02:21 +0100

    Added displayForecast function and ijected html inside it, made a loop of six days

--- src/index.js
+++ src/index.js
@@ -31,28 +31,43 @@ function showDate() {
 
 document.querySelector("#todaysDate").innerHTML = showDate();
 
+function formatForcastDate(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[day];
+}
+
 function displayForecast(response) {
-  console.log(response.data.daily);
+  let forecast = response.data.daily;
   let forecastElement = document.querySelector("#forecast");
   let forecastHTML = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="forecast-day">${day}</div>
-      <img
-        src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+      <div class="forecast-day">${formatForcastDate(forecastDay.dt)}</div>
+      <img  
+        src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"
         alt="icon"
         width="42"
       />
       <div class="forecast-temperatures">
-        <span class="forecast-temperature-max">${response.data.daily}°</span>
-        <span class="forecast-temperature-min">${response.data.daily}°</span>
+        <span class="forecast-temperature-max">${Math.round(
+          forecastDay.temp.max
+        )}°</span>/
+        <span class="forecast-temperature-min">${Math.round(
+          forecastDay.temp.min
+        )}°</span>
       </div>
     </div>
   `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
