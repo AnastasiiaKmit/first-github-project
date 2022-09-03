86063273608c02233f367c14612eb9480733ff28
AnastasiiaKmit <anastasiiakmit99@gmail.com>
2022-09-03 22:26:02 +0100

    Deleted fahrenheit link and converting functions

--- src/index.js
+++ src/index.js
@@ -13,23 +13,17 @@ if (minutes < 10) {
 }
 
 function showDate() {
-  //let date = now.getDate();
-  //if (date < 10) {
-  //date = `0${date}`;
-  //}
-  //let month = now.getMonth() + 1;
-  //if (month < 10) {
-  //month = `0${month}`;
-  //}
-  //let year = now.getFullYear();
-  return [
-    `${dayOfWeek(now.getDay())} `,
-    `${hour}:${minutes}`,
-    //`${date}/${month}/${year}`,
-  ].join(" ");
+  return [`${dayOfWeek(now.getDay())} `, `${hour}:${minutes}`].join(" ");
 }
 
 document.querySelector("#todaysDate").innerHTML = showDate();
+
+function search(event) {
+  event.preventDefault();
+  let cityElement = document.querySelector("#inputtedCity");
+  let cityInput = document.querySelector("#inputCity");
+  cityElement.innerHTML = cityInput.value;
+}
 
 function formatForcastDate(timestamp) {
   let date = new Date(timestamp * 1000);
@@ -78,13 +72,13 @@ function getForecast(coordinates) {
   console.log(coordinates);
   let apiKey = "a221b0bc8a3d0f29a2938e05fc8d1784";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayForecast);
 }
 
 function showTempInputtedCity(response) {
   let iconElement = document.querySelector("#icon");
   let icon = response.data.weather[0].icon;
+
   celsiusTemperature = Math.round(response.data.main.temp);
   document.querySelector(
     "#inputtedCity"
@@ -133,40 +127,19 @@ function showPosition(position) {
   let latitude = position.coords.latitude;
   let longitude = position.coords.longitude;
   let apiKey = "0fd0719178975f973227695c5ae18796";
-  let units = "metric";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showTemperature);
 }
 
-function getCurrentPosition() {
+function getCurrentPosition(event) {
+  event.preventDefault();
   navigator.geolocation.getCurrentPosition(showPosition);
 }
 
 let currentButton = document.querySelector("#back_to_current_city");
 currentButton.addEventListener("click", getCurrentPosition);
 
-function displayFahrenheitTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
-  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event) {
-  event.preventDefault();
-  let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature = null;
-
 let form = document.querySelector("#form-input");
 form.addEventListener("submit", handleSubmit);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 searchCity("Lviv");
